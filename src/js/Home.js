047c21373226637235ef104a62b28ba0dc956b84
Pavel Baranchuk <pbaranchuk@gmail.com>
2018-10-10 13:21:08 +0300

    Basic setup

---
+++ src/js/Home.js
@@ -0,0 +1,43 @@
+export class Home {
+  constructor(address, color, postalCode) {
+    this._address = address;
+    this._color = color;
+    this._postalCode = postalCode;
+    this._devices = [];
+  }
+
+  get address() {
+    return this._address;
+  }
+
+  get color() {
+    return this._address;
+  }
+
+  get postalCode() {
+    return this._postalCode;
+  }
+
+  get devices() {
+    return this._devices;
+  }
+
+  addDevice(device) {
+    this._devices.push(device);
+  }
+
+  removeDevice(device) {
+    let index = this._devices.indexOf(device);
+    this._devices.splice(index, 1);
+  }
+
+  getDeviceByNamer(name) {
+    let res;
+    this._devices.forEach(device => {
+      if (device.name === name) {
+        res = device;
+      }
+    });
+    return res;
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,6 @@
 import "../scss/index.scss";
-import Animal from "./Animal";
-import Zoo from "./Zoo";
-
-const zoo = new Zoo("TomZoo", "TYT");
-
-window.zoo = zoo;
-
-zoo.addAnimal(new Animal("Jerry", 1, "M"));
-zoo.addAnimal(new Animal("Leopold", 5, "M"));
-zoo.addAnimal(new Animal("Begemot", 3, "FM"));
+import Home from "./Home";
+import Appliance from "./Appliance";
+import Light from "./Appliance/Light";
+import Coffee from "./Appliance/Coffee";
+import Security from "./Appliance/Security";
