441b54a517341fa9eedd7344dd158a5e37d50a25
Pavel Baranchuk <pbaranchuk@gmail.com>
2018-10-17 12:52:46 +0300

    slight changes in a model

--- src/index.js
+++ src/index.js
@@ -5,4 +5,9 @@ import Tv from "./js/Tv";
 
 const home = new Home("Kiev, Bazhanova,app.7", 61000, "Petro");
 console.log(home);
-const light = new Light(undefined, "yellow_color");
+const light = new Light("Light", "Default text");
+console.log(light);
+const tv = new Tv("LG flatron w2243s", "Default text");
+console.log(tv);
+const coffeeMachine = new CoffeeMachine("Coffee Machine", "Default text");
+console.log(coffeeMachine);
